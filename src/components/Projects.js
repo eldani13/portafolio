ce2ec32d253b8c43fcba3a1ddbfca32383743cbe
eldani13 <ll-luis123@hotmail.com>
2024-04-18 14:53:21 -0500

    cambios

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,8 @@ import projImg3 from "../assets/img/3.png";
 import projImg4 from "../assets/img/4.png";
 import projImg5 from "../assets/img/5.png";
 import projImg6 from "../assets/img/6.png";
+import projImg7 from "../assets/img/dias.png"
+import projImg8 from "../assets/img/propina.png"
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -14,34 +16,40 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Software para buses",
+      title: "Software Proveedor restaurante",
       description: "React, TS, Next.js, Tailwind",
       imgUrl: projImg1,
+      linkUrl: "https://rutas-main.vercel.app/"
     },
     {
       title: "Langind page Red-Social Vopper",
       description: "React, TS, Next.js, Tailwind",
       imgUrl: projImg2,
+      linkUrl: "https://vopper.vercel.app/",
     },
     {
       title: "Pagina e-Commerce Full-Movil",
       description: "React, TS, Next.js, Tailwind",
       imgUrl: projImg3,
+      linkUrl: "https://fullmovilstore.com/",
     },
     {
       title: "Chat Vopper",
       description: "React, TS, Next.js, Tailwind",
       imgUrl: projImg4,
+      
     },
     {
-      title: "Gestor de tarea",
-      description: "React, TS, Next.js, Tailwind",
-      imgUrl: projImg5,
+      title: "Calculador de dias",
+      description: "HTML, CSS, JS",
+      imgUrl: projImg7,
+      linkUrl: "https://days-calculator-iota.vercel.app/"
     },
     {
-      title: "Spotify-clone",
-      description: "React, TS, Next.js, Tailwind",
-      imgUrl: projImg6,
+      title: "Calcular propina",
+      description: "HTML, CSS, JS",
+      imgUrl: projImg8,
+      linkUrl: "https://tip-sandy.vercel.app/"
     },
   ];
 
@@ -67,7 +75,7 @@ export const Projects = () => {
                       <Nav.Link eventKey="third">Seccion 3</Nav.Link>
                     </Nav.Item>
                   </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""} style={{cursor: "pointer"}}>
                     <Tab.Pane eventKey="first">
                       <Row>
                         {
